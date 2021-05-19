1051289fceb1874668bf4cb97ac48e6f8c9b2c8b
Yong Xia <xywork008@gmail.com>
2021-05-19 10:03:02 -0500

    set up mongodb altas with strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4f758884187b3c3562797252c8160ce5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '636d7aae-02eb-4340-8ea3-b7e800fa9528'
+};
